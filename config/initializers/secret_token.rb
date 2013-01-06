6a348130493e98e5782a30dffe0b20fba7024271
Arnaud Besnier <arnaudIbesnier@gmail.com>
2013-01-06 12:59:18 +0100

    rails new trades_watcher

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TradesWatcher::Application.config.secret_token = 'ef87438bdfc5fe2bf02ecbe43ddcc86f2e79a81eb9d5340ad319ceda56e6586c5ec276acf52f1a9cf42ee1112974fdef1f0443d047e123cd419a847c58dc0701'
