2a7e43c8c316045f416e36ab1253fe8aeec8b6bc
Arnaud Besnier <arnaudIbesnier@gmail.com>
2013-01-10 21:41:13 +0100

    Do not launch the scheduler on console

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -1,24 +1,27 @@
 scheduler = Rufus::Scheduler.start_new
 
-def market_is_opened? datetime
-	week_day    = datetime.wday
-	hour        = datetime.hour
-	is_week_day = [1,2,3,4,5].include?(datetime.wday) # From Monday to Friday
-	
-	is_week_day && hour > 9 && hour < 18
-end
-
-# Retrieve near-realtime quotes
-
-# TODO: stop the scheduler when the market is closed
-#       (from 9:00 to 18:00)
-scheduler.every '10s' do
-	now = Time.now
-	if market_is_opened? now
-		puts " = #{now} => Retrieve Yahoo Finance data\n\n"
-		Portfolio.new
-		puts " ===========================================\n\n"
-	else
-		puts " = #{now} => The market is currently closed"
+# Avoid scheduler for Rails console
+if !defined? Rails::Console
+	def market_is_opened? datetime
+		week_day    = datetime.wday
+		hour        = datetime.hour
+		is_week_day = [1,2,3,4,5].include?(datetime.wday) # From Monday to Friday
+		
+		is_week_day && hour > 9 && hour < 18
 	end
-end 
+
+	# Retrieve near-realtime quotes
+
+	# TODO: stop the scheduler when the market is closed
+	#       (from 9:00 to 18:00)
+	scheduler.every '10s' do
+		now = Time.now
+		if market_is_opened? now
+			puts " = #{now} => Retrieve Yahoo Finance data\n\n"
+			Portfolio.new
+			puts " ===========================================\n\n"
+		else
+			puts " = #{now} => The market is currently closed"
+		end
+	end
+end
