18419b8a9a7c07f36f5f3b4bee214cf0772e81e0
Arnaud Besnier <arnaudIbesnier@gmail.com>
2013-01-10 22:18:19 +0100

    Simplify the scheduler

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -2,26 +2,12 @@ scheduler = Rufus::Scheduler.start_new
 
 # Avoid scheduler for Rails console
 if !defined? Rails::Console
-	def market_is_opened? datetime
-		week_day    = datetime.wday
-		hour        = datetime.hour
-		is_week_day = [1,2,3,4,5].include?(datetime.wday) # From Monday to Friday
-		
-		is_week_day && hour > 9 && hour < 18
-	end
-
 	# Retrieve near-realtime quotes
-
-	# TODO: stop the scheduler when the market is closed
-	#       (from 9:00 to 18:00)
-	scheduler.every '10s' do
+	# when the market is opened (from 9:00 to 18:00)
+	scheduler.cron '* 9-18 * * 1-5 Europe/Paris' do
 		now = Time.now
-		if market_is_opened? now
-			puts " = #{now} => Retrieve Yahoo Finance data\n\n"
-			Portfolio.new
-			puts " ===========================================\n\n"
-		else
-			puts " = #{now} => The market is currently closed"
-		end
+		puts " = #{now} => Retrieve Yahoo Finance data\n\n"
+		Portfolio.new
+		puts " ===========================================\n\n"
 	end
 end
