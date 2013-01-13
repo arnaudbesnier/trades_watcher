6a1d5e52c2c02e7f7c6d1cbf8796764946282cd4
Arnaud Besnier <arnaudIbesnier@gmail.com>
2013-01-13 14:32:14 +0100

    Simplify data in the quotes admin

--- app/admin/quotes.rb
+++ app/admin/quotes.rb
@@ -5,13 +5,13 @@ ActiveAdmin.register Quote do
 	filter :company
 
 	index :download_links => false do
-		column(:company)        { |quote| quote.company.name }
-    	column(:value)          { |quote| format_price(quote.value, 3) }
-    	column(:variation_day)  { |quote| format_variation(quote.variation_day_current) }
-    	column(:volume)         { |quote| format_integer(quote.volume) }
-    	column(:value_day_open) { |quote| format_price(quote.value_day_open, 3) }
-    	column(:values_day)     { |quote| "#{quote.value_day_low} - #{quote.value_day_high}" }
-    	column(:variations_day) { |quote| "#{quote.variation_day_low} - #{quote.variation_day_high}" }
+		column(:company)       { |quote| quote.company.name }
+    	column(:value)         { |quote| format_price(quote.value, 3) }
+    	column(:variation_day) { |quote| format_variation(quote.variation_day_current) }
+    	column(:volume)        { |quote| format_integer(quote.volume) }
+    	column(:day_open)      { |quote| format_price(quote.value_day_open, 3) }
+    	column(:day_low)       { |quote| format_variation(quote.variation_day_low) }
+    	column(:day_high)      { |quote| format_variation(quote.variation_day_high) }
     	column :created_at
   	end
 
