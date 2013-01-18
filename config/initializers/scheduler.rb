317c2b34b09eb147d05ef09c9ebd6e35e42cdedd
Arnaud Besnier <arnaudIbesnier@gmail.com>
2013-01-18 22:50:19 +0100

    Indent initializers

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -2,11 +2,11 @@ scheduler = Rufus::Scheduler.start_new
 
 # Avoid scheduler for Rails console
 if !defined? Rails::Console
-	# Retrieve near-realtime quotes
-	# when the market is opened (from 9:00 to 18:00)
-	scheduler.cron '* 9-18 * * 1-5 Europe/Paris' do
-		now = Time.now
-		puts " = #{now} => Retrieve Yahoo Finance data\n\n"
-		Portfolio.new
-	end
+  # Retrieve near-realtime quotes
+  # when the market is opened (from 9:00 to 18:00)
+  scheduler.cron '* 9-18 * * 1-5 Europe/Paris' do
+    now = Time.now
+    puts " = #{now} => Retrieve Yahoo Finance data\n\n"
+    Portfolio.new
+  end
 end
