0a295fc859d7682d70b59e59b5fb042f58439034
Arnaud Besnier <arnaudIbesnier@gmail.com>
2013-01-18 22:47:15 +0100

    Indentation

--- app/admin/companies.rb
+++ app/admin/companies.rb
@@ -1,20 +1,21 @@
 ActiveAdmin.register Company do
+
   config.sort_order = :name_asc
 
-    filter :symbol
+  filter :symbol
 
-    index :download_links => false do
-      column :name
-      column(:symbol)        { |company| link_to company.symbol, admin_company_path(company) }
-      column(:current_value) { |company| company.quotes.any? ? format_price(company.quotes.last.value) : nil }
+  index :download_links => false do
+    column :name
+    column(:symbol)        { |company| link_to company.symbol, admin_company_path(company) }
+    column(:current_value) { |company| company.quotes.any? ? format_price(company.quotes.last.value) : nil }
+  end
+
+  show do |company|
+    attributes_table do
+      row :name
+      row :symbol
+      row(:current_value) { |company| company.quotes.any? ? format_price(company.quotes.last.value) : nil }
     end
-
-  	show do |company|
-	  attributes_table do
-		row :name
-		row :symbol
-		row(:current_value) { |company| company.quotes.any? ? format_price(company.quotes.last.value) : nil }
-  	  end
-  	end
+  end
 
 end
--- app/admin/quotes.rb
+++ app/admin/quotes.rb
@@ -1,21 +1,21 @@
 ActiveAdmin.register Quote do
 
-	config.sort_order = :company_id_asc
+  config.sort_order = :company_id_asc
 
-    actions :index, :show
+  actions :index, :show
 
-	filter :company
+  filter :company
 
-	index :download_links => false do
-		column(:company)       { |quote| link_to quote.company.name, admin_company_path(quote.company_id) }
-    	column(:value)         { |quote| format_price(quote.value, 3) }
-    	column(:variation_day) { |quote| format_variation(quote.variation_day_current) }
-        column(:gain_day)      { |quote| format_variation_price(quote.variation_price_current) }
-    	column(:volume)        { |quote| format_integer(quote.volume) }
-        column(:day_open)      { |quote| format_price(quote.value_day_open, 3) }
-        column(:day_low)       { |quote| format_variation(quote.variation_day_low) }
-    	column(:day_high)      { |quote| format_variation(quote.variation_day_high) }
-    	column :created_at
-  	end
+  index :download_links => false do
+    column(:company)       { |quote| link_to quote.company.name, admin_company_path(quote.company_id) }
+    column(:value)         { |quote| format_price(quote.value, 3) }
+    column(:variation_day) { |quote| format_variation(quote.variation_day_current) }
+    column(:gain_day)      { |quote| format_variation_price(quote.variation_price_current) }
+    column(:volume)        { |quote| format_integer(quote.volume) }
+    column(:day_open)      { |quote| format_price(quote.value_day_open, 3) }
+    column(:day_low)       { |quote| format_variation(quote.variation_day_low) }
+    column(:day_high)      { |quote| format_variation(quote.variation_day_high) }
+    column :created_at
+  end
 
 end
