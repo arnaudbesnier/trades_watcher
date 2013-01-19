128d2732bc887535bb8f96f884f547d6ac03f00c
Arnaud Besnier <arnaudIbesnier@gmail.com>
2013-01-19 14:24:08 +0100

    Set the default_url_options in production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,6 +48,7 @@ TradesWatcher::Application.configure do
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
 
+  config.action_mailer.default_url_options = { :host => 'trades-watcher.herokuapp.com' }
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
