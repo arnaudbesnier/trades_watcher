c348ce5775d1c3eae33845ef94a6d5b00edcdc71
Arnaud Besnier <arnaudIbesnier@gmail.com>
2013-01-19 14:39:07 +0100

    Add a complete config for the mailer in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,9 +48,20 @@ TradesWatcher::Application.configure do
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
 
+  # Action Mailer config
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address        => "smtp.sendgrid.net",
+    :port           => "25",
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => ENV['SENDGRID_DOMAIN']
+  }
   config.action_mailer.default_url_options = { :host => 'trades-watcher.herokuapp.com' }
-  # Disable delivery errors, bad email addresses will be ignored
-  # config.action_mailer.raise_delivery_errors = false
+
+  # We care if the mailer can't send
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable threaded mode
   # config.threadsafe!
