358166c3d623dd93eb0d4e5c739bf359b5e07e62
Arnaud Besnier <arnaudIbesnier@gmail.com>
2013-02-10 19:45:27 +0100

    Add orders admin detail page

--- app/admin/orders.rb
+++ app/admin/orders.rb
@@ -23,6 +23,14 @@ ActiveAdmin.register Order do
 
   show do |company|
     attributes_table do
+    row(:company)     { |order| link_to order.company.name, admin_company_path(order.company_id) }
+    row(:shares)      { |order| format_integer(order.shares, { :right_align => false }) }
+    row(:price)       { |order| format_price(order.price, { :right_align => false }) }
+    row(:type)        { |order| format_status(Order::TYPE_NAMES[order.order_type], { :color => Order::TYPE_COLORS[order.order_type] }) }
+    row(:commission)  { |order| format_price(order.commission, { :right_align => false }) }
+    row(:taxes)       { |order| format_price(order.taxes, { :right_align => false }) if order.order_type == Order::BUY }
+    row(:created_at)  { |order| link_to format_datetime(order.created_at), admin_order_path(order.id) }
+    row(:executed_at) { |order| link_to format_datetime(order.executed_at), admin_order_path(order.id) if order.executed }
     end
   end
 
