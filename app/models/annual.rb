30fb2c69a2ca2b04cd98543ee4fc5884a00dfc91
Arnaud Besnier <arnaudIbesnier@gmail.com>
2013-02-16 13:25:34 +0100

    Refact the Annual model

--- app/models/annual.rb
+++ app/models/annual.rb
@@ -9,25 +9,20 @@ class Annual
   	@year_current_datetime = Time.new(@year)
   	@year_next_datetime    = Time.new(@year + 1)
 
-  	dividendes_year  = Dividend.where('received_at > ? AND received_at < ?', @year_current_datetime, @year_next_datetime)
-  	trade_gains_year = Trade.joins('LEFT OUTER JOIN orders ON orders.id = trades.order_close_id').where('orders.executed_at > ? AND orders.executed_at < ?', @year_current_datetime, @year_next_datetime)
-  	deposits_year    = Transaction.where('created_at > ? AND created_at < ?', @year_current_datetime, @year_next_datetime)
+    begin_year = Time.new(2011)
 
-  	dividendes_total  = Dividend.where('received_at < ?', @year_next_datetime)
+    @deposits       = Transaction.deposit_total(@year_current_datetime, @year_next_datetime)
+    @deposits_total = Transaction.deposit_total(begin_year, @year_next_datetime)
+
+    @dividends       = Dividend.total_net(@year_current_datetime, @year_next_datetime)
+    @dividends_total = Dividend.total_net(begin_year, @year_next_datetime)
+
+    @trade_gains, @trade_gains_total = 0, 0
+  	trade_gains_year  = Trade.joins('LEFT OUTER JOIN orders ON orders.id = trades.order_close_id').where('orders.executed_at > ? AND orders.executed_at < ?', @year_current_datetime, @year_next_datetime)
   	trade_gains_total = Trade.joins('LEFT OUTER JOIN orders ON orders.id = trades.order_close_id').where('orders.executed_at < ?', @year_next_datetime)
-  	deposits_total    = Transaction.where('created_at < ?', @year_next_datetime)
-
-  	@dividends, @trade_gains, @deposits                   = 0, 0, 0
-  	@dividends_total, @trade_gains_total, @deposits_total = 0, 0, 0
-  	@closings = trade_gains_year.count
-
-  	dividendes_year.each  { |dividend| @dividends   += dividend.total_net }
-  	trade_gains_year.each { |trade|    @trade_gains += trade.gain }
-  	deposits_year.each    { |deposit|  @deposits    += deposit.amount }
-
-  	dividendes_total.each  { |dividend| @dividends_total   += dividend.total_net }
-  	trade_gains_total.each { |trade|    @trade_gains_total += trade.gain }
-  	deposits_total.each    { |deposit|  @deposits_total    += deposit.amount }
+  	@closings         = trade_gains_year.count
+    trade_gains_year.each  { |trade| @trade_gains       += trade.gain }
+    trade_gains_total.each { |trade| @trade_gains_total += trade.gain }
  
     @trade_gains_performance = @trade_gains / @deposits_total * 100
     @dividends_performance   = @dividends / @deposits_total * 100
