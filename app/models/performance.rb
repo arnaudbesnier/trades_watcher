25047884f427b3f6dd33d1ebfbbd6573a7f56a28
Arnaud Besnier <arnaudIbesnier@gmail.com>
2013-02-17 02:54:15 +0100

    Clean the performance model

--- app/models/performance.rb
+++ app/models/performance.rb
@@ -9,15 +9,15 @@ class Performance
     begin_story, now = Time.new(2011), Time.now
     period_size      = end_date - begin_date
 
-    @deposits      = Transaction.deposit_total(begin_date, end_date)
-    @dividends_net = Dividend.total_net(begin_story, end_date)
-    @closings      = Trade.closed_between(begin_date, end_date).count
-    @trade_gains   = Trade.sold_stock_gain(begin_date, end_date)
-    @stock_value   = Trade.stock_value(end_date)
+    @deposits    = Transaction.deposit_total(begin_date, end_date)
+    @closings    = Trade.closed_between(begin_date, end_date).count
+    @trade_gains = Trade.sold_stock_gain(begin_date, end_date)
+    @stock_value = Trade.stock_value(end_date)
 
     @deposits_total    = Transaction.deposit_total(begin_story, end_date)
+    @dividends_net     = Dividend.total_net(begin_story, end_date)
     @trade_gains_total = Trade.sold_stock_gain(begin_story, end_date)
-    @liquidity         = @deposits_total + @dividends_net + @trade_gains_total - Trade.stock_purchase_value#(end_date)
+    @liquidity         = @deposits_total + @dividends_net + @trade_gains_total - Trade.stock_purchase_value
 
     @valorization       = @stock_value + @liquidity
     @performance_period = @stock_value - Trade.stock_value(end_date - period_size)
