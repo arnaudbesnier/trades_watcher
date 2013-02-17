28c0277be3b3d3b425d93eb15f880348c569498f
Arnaud Besnier <arnaudIbesnier@gmail.com>
2013-02-17 02:20:48 +0100

    Fix order detail display

--- app/admin/orders.rb
+++ app/admin/orders.rb
@@ -31,7 +31,7 @@ ActiveAdmin.register Order do
     row(:type)        { |order| format_status(Order::TYPE_NAMES[order.order_type], { :color => Order::TYPE_COLORS[order.order_type] }) }
     row(:commission)  { |order| format_price(order.commission, { :right_align => false }) }
     row(:taxes)       { |order| format_price(order.taxes, { :right_align => false }) if order.order_type == Order::BUY }
-    row(:total)       { |order| format_price(order.total) }
+    row(:total)       { |order| format_price(order.total, { :right_align => false }) }
     row(:created_at)  { |order| link_to format_datetime(order.created_at), admin_order_path(order.id) }
     row(:executed_at) { |order| link_to format_datetime(order.executed_at), admin_order_path(order.id) if order.executed }
     end
