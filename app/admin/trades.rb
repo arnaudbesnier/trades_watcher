7b80e6908f2c07cb1760a55227ac4bd65cb72ac3
Arnaud Besnier <arnaudIbesnier@gmail.com>
2013-02-20 23:00:41 +0100

    Add a sidebar in the trades index

--- app/admin/trades.rb
+++ app/admin/trades.rb
@@ -25,7 +25,7 @@ ActiveAdmin.register Trade do
     column(:market_value) { |trade| format_price(trade.current_value) }
     column(:gain)         { |trade| format_price_and_variation(trade.gain, trade.performance, { :highlight => trade.opened? }) }
     column('Day\'s Gain') { |trade| format_price_with_sign(trade.gain_day, { :highlight => trade.opened? }) }
-    column(:loss_max)     { |trade| format_price_and_variation(trade.max_loss, trade.max_loss_variation) if trade.opened? }
+    column(:risk_max)     { |trade| format_price_and_variation(trade.max_loss, trade.max_loss_variation) if trade.opened? }
   end
 
   show do |trade|
@@ -41,6 +41,22 @@ ActiveAdmin.register Trade do
     end
   end
 
+  sidebar :current_situation, :only => :index do
+    current_performance = Performance.new
+
+    begin_day = Date.today
+    end_day   = Date.today + 1.day
+    day_performance = Performance.new(begin_day, end_day)
+
+    attributes_table_for current_performance do
+      row(:stock_value)  { format_price(current_performance.stock_value) }
+      row(:day_gain)     { format_price_and_variation(day_performance.performance_period, day_performance.variation_period) }
+      row(:valorization) { format_price(current_performance.valorization) }
+      row(:total_gain)   { format_price_and_variation(current_performance.performance_total, current_performance.variation_total) }
+      row(:risk_max)     { format_price_and_variation(*Trade.max_loss_and_ratio) }
+    end
+  end
+
   sidebar :today, :only => :show do
     attributes_table_for trade do
       row(:last_price)   { |trade| format_price(trade.company.last_value) }
@@ -52,7 +68,7 @@ ActiveAdmin.register Trade do
   sidebar :other, :only => :show do
     attributes_table_for trade do
       row(:fees)     { |trade| format_price(trade.total_fees) }
-      row(:loss_max) { |trade| format_price_and_variation(trade.max_loss, trade.max_loss_variation) } if trade.opened?
+      row(:risk_max) { |trade| format_price_and_variation(trade.max_loss, trade.max_loss_variation) } if trade.opened?
     end
   end
 
