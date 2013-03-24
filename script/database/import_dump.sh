6259a96a9f01fe8964a392929819e8a717900483
Arnaud Besnier <arnaudIbesnier@gmail.com>
2013-03-24 11:47:21 +0100

    Add a script to import the production database for development

---
+++ script/database/import_dump.sh
@@ -0,0 +1,15 @@
+# If not backup path specified, use the default one
+if [ "$1" ]; then
+	BACKUP="$1"
+else
+	BACKUP="~/Dropbox/Documents/Money/Trades-watcher/last.dump"
+fi
+
+echo '--- Clear schema'
+psql -U postgres -d trades_watcher_development << EOF
+  DROP SCHEMA IF EXISTS public CASCADE
+EOF
+
+# Restore db (see http://devcenter.heroku.com/articles/pgbackups#exporting_via_a_backup)
+echo '--- Restore dump'
+pg_restore --verbose --clean --no-acl --no-owner  -U postgres -d trades_watcher_development "$BACKUP"
